8294cf016ac27c8e6ae78acdbb1353febaa76fbd
Paula Tulis <tulis.paula@gmail.com>
2019-03-17 16:39:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+#returns a string without any additional formatting when given a 1-element array 
+if array.length == 1
+  return array.join
+  #adds 'and' between elements when given a 2-element array
+elsif array.length == 2 
+return array.join(" and ")
+elsif array.length >= 3
+and_comma = "and #{array[-1]}"
+array.pop
+array.push (and_comma)
+return array.join(", ")
+end
 end
