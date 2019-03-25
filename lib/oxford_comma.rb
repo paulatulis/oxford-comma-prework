aab1defe4c556633f28ed892f37560c98d77f3f8
Paula Tulis <tulis.paula@gmail.com>
2019-03-25 23:54:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
+
 #returns a string without any additional formatting when given a 1-element array
 
 #adds 'and' between elements when given a 2-element array
+
+def oxford_comma(array)
+  while array.length = 1 
+  array.to_s 
+end 
